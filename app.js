7a9adc4ca7fc233391f67b6371445f87f2a651d2
Giriid <105373009+Giriid@users.noreply.github.com>
2023-04-07 21:11:33 -0500

    somewhat working

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-var express = require("express");
+var express = require('express');
+var fs = require('fs');
+var parser = require('body-parser');
 var app = express();
 var router = express.Router();
+var ticketList;
+port = 3000;
+
+app.use(parser.json());
+
+fs.readFile('./testTicket.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.log('Error in readfile: ', err);
+    } else {
+        ticketList = JSON.parse(data);
+    }
+});
+
+router.get('/', function(req, res) {
+    console.log('Start page!');
+});
+
+router.get('/list', (req, res) => {
+    res.json(ticketList);
+});
+
+router.get('/ticket/:id', (req, res) => {
+    res.json(ticketList.req.params.d);
+});
+
+router.post('/new-tickets', (req, res) => {
+    var body = req.body;
+    var id = 0;
+    for (var i in ticketList) {
+        id++;
+    }
+    body.id = id;
+    ticketList.id = body;
+    res.status(201).json(ticketList.id);
+});
 
 app.use(router);
-
-app.set("port", process.env.PORT || 3000);
-app.listen(app.get("port"), function() {
-    console.log("Server started on port " + app.get("port"));
-});
-
-router.get("/test", function(req, res) {
-    res.status(200).send('Welcome to "/test"!')
-});
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Server started at http://localhost:', port)
+    }
+})
